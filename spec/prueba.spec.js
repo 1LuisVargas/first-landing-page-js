a167b5c40306679454c39b4a33ebf020766fa4f0
Luis Vargas <luisvargas98@hotmail.com>
2024-11-30 12:43:03 -0500

    Adding the homework for Jasmine activity

--- spec/prueba.spec.js
+++ spec/prueba.spec.js
@@ -20,7 +20,7 @@ class ToDoList {
   }
 }
 
-//Testing
+//Testing the sample exercise done in the class video
 describe('Testing ToDoList', () => {
   //Creating an instance of ToDoList before every test
   beforeEach(() => {
@@ -53,3 +53,72 @@ describe('Testing ToDoList', () => {
     expect(testingList.list()).not.toContain('Test4');
   })
 });
+
+class Activities {	
+  constructor(id, title, description, image) {
+      this.id = id;
+      this.title = title;
+      this.description = description;
+      this.image = image;        
+  }
+}
+
+class ActivityList {
+  // Creating the empty array to store all activities
+  constructor() {
+      this.activities = [];
+  }
+
+  // New method to add activity to the array
+  addActivity(activity) {
+      activity.id = this.activities.length + 1;
+      this.activities.push(activity);
+  }
+
+  // Method to get all activities
+  getActivities() {
+      return this.activities;
+  }
+
+  //Method to get activity by id and remove it from the array 
+  deleteActivity(id) {
+      this.activities = this.activities.filter(activity => activity.id !== id);
+  }
+}
+
+// Testing the activityList class and everything we've done so far
+describe('Testing ActivityList', () => {
+  //Creating an instance of ActivityList before every test
+  beforeEach(() => {
+    activityList1 = new ActivityList();
+  })
+
+  //Testing if the ActivityList is a class
+  it('Must be a class', () => {
+    expect(typeof ActivityList.prototype.constructor).toBe('function');
+  });
+
+  //Testing if the new ActivityList is an instance of the ActivityList class
+  it('Every list should be an instance of ActivityList', () => {
+    expect(new ActivityList()).toBeInstanceOf(ActivityList);
+  });
+
+  //Testing if addActivity is able to add an element
+  it('Is it able to add an element?', () => {
+    activityList1.addActivity(new Activities(1, 'Test', 'Test', 'Test'));
+    expect(activityList1.getActivities()).toContain(new Activities(1, 'Test', 'Test', 'Test'));    
+  });
+
+  //Testing if deleteActivity is able to remove the last element
+  it('Is it able to remove the last element?', () => {
+    activityList1.addActivity(new Activities(1, 'Test', 'Test', 'Test'));
+    activityList1.deleteActivity(1);
+    expect(activityList1.getActivities()).not.toContain(new Activities(1, 'Test', 'Test', 'Test'));
+  })
+
+  //Testing if getActivities is able to get all elements
+  it('Is it able to get all elements?', () => {
+    activityList1.addActivity(new Activities(1, 'Test', 'Test', 'Test'));
+    expect(activityList1.getActivities()).toContain(new Activities(1, 'Test', 'Test', 'Test'));
+  })
+})
